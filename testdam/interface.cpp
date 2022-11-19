01c80145c18a09caa32007eb7aaf890535b4b80f
Damien GARCIA <damien.garcia@etu.univ-nantes.fr>
2022-11-19 15:35:51 +0100

    Working on database system. It still has weird behavior, so partially working.

--- testdam/interface.cpp
+++ testdam/interface.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-// #include <unistd.h>
-// #include <termios.h>
-// #include <stdio.h>
+
 
 #include "user.hpp"
 #include "patient.hpp"
@@ -11,53 +9,6 @@
 #include "radiography.hpp"
 #include "report.hpp"
 #include "snapshot.hpp"
-
-
-// int getch() {
-//     int ch;
-//     struct termios t_old, t_new;
-
-//     tcgetattr(STDIN_FILENO, &t_old);
-//     t_new = t_old;
-//     t_new.c_lflag &= ~(ICANON | ECHO);
-//     tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
-
-//     ch = getchar();
-
-//     tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
-//     return ch;
-// }
-
-
-// std::string getpass(const char *prompt, bool show_asterisk=true) {
-//     const char BACKSPACE=127 ;
-//     const char RETURN=10 ;
-
-//     std::string password ;
-//     unsigned char ch=0 ;
-
-//     std::cout << prompt << std::endl ;
-
-//     while ((ch=getch()) != RETURN) {
-//        if (ch == BACKSPACE) {
-//             if (password.length() != 0) {
-//                 if (show_asterisk) {
-//                     std::cout <<"\b \b" ;
-//                     password.resize(password.length()-1) ;
-//                 }
-//             } else {
-//                 password+=ch;
-//                 if(show_asterisk) {
-//                     std::cout <<'*' ;
-//                 }
-//             }
-//         }
-//     }
-//     std::cout << std::endl ;
-//     return password;
-// }
-
-
 
 
 
--- testdam/main.cpp
+++ testdam/main.cpp
@@ -6,32 +6,35 @@
 #include "report.hpp"
 #include "snapshot.hpp"
 #include "interface.cpp"
-
+#include "database.hpp"
 
 int main() {
+    DatabaseHandling db ;
+
     // Some patients
     Patient p1("Damien", "Garcia", "123456789") ;
     Patient p2("Florian", "Echelard", "987654321") ;
+    db.add_patient(p1) ;
+    db.add_patient(p2) ;
 
     // Some doctors
     Doctor d1("Lucas", "David", "748159263") ;
     Doctor d2("Cédric", "Guerineau", "326159487") ;
-    
-    // Some radiographies 
-    Radiography r1(xRay, pending, 3, 2, 2023) ;
-    Radiography r2(ultrasound, done, 4, 3, 2021) ;
-    r2.add_snap("554468", "path/to/file1.txt") ;
-    r2.add_snap("115654", "path/to/file2.txt") ;
-    r2.add_snap("654813", "path/to/file3.txt") ;
-    r2.add_report("This is a report you shoudl take seriously...", "dontLookMyPassword") ;
+    db.add_doctor(d1) ;
+    db.add_doctor(d2) ;
 
-    menu() ;
-    // std::cout << r1.get_day() << std::endl ;
-    // std::cout << r1.get_month() << std::endl ;
-    // std::cout << r1.get_year() << std::endl ;
-    // // std::cout << r1.get_report().get_content("dontLookMyPassword") << std::endl ;
-    // system("clear") ;
-    // std::cout << "menu is clean" << std::endl ;
+    // // Some radiographies 
+    // Radiography r1(xRay, pending, 3, 2, 2023) ;
+    // Radiography r2(ultrasound, done, 4, 3, 2021) ;
+    // r2.add_snap("554468", "path/to/file1.txt") ;
+    // r2.add_snap("115654", "path/to/file2.txt") ;
+    // r2.add_snap("654813", "path/to/file3.txt") ;
+    // r2.add_report("This is a report you shoudl take seriously...", "dontLookMyPassword") ;
+
+    // menu() ;
+
+    db.update_doctors_database() ;
+    db.update_patients_database() ;
 
     return 0 ;
 }
